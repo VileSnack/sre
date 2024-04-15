f96f9b8c9e4a3432e0e1570cfbee0be06eea01fd
John VanSickle <evilsnack@hotmail.com>
2024-04-14 23:51:43 -0500

    Fixed an issue with parsing the command line for the ifg utility.

--- ifg/parse.c
+++ ifg/parse.c
@@ -3,6 +3,7 @@
 //
 // Parses the command line.
 //
+#include <limits.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "funcs.h"
@@ -20,197 +21,236 @@ bool ParseCommandLine(int argc, char** argv)
 
 	for (index = 1; index < argc; index++)
 	{
-		if (0 == strcmp("-l", argv[index]))
+		if ('-' != argv[index][0])
 		{
-			index++;
+			fprintf(stderr,
+				"***** Error: '%s' is not a valid option.\n",
+				argv[index]
+			);
 
-			if (index == argc)
-			{
+			return false;
+		}
+
+		switch (argv[index][1])
+		{
+			case 'l':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the left column.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &xMin) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -l option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+			break;
+
+			case 'r':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the right column.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &xMax) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -r option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+			break;
+
+			case 't':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the top row.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &yMin) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -t option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+			break;
+
+			case 'b':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the bottom row.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &yMax) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -b option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+			break;
+
+			case 'w':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the width.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &xMax) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -w option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+
+				xMax--;
+				xMin = 0;
+			break;
+
+			case 'h':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the height.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &yMax) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -h option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+
+				yMax--;
+				yMin = 0;
+			break;
+
+			case 'a':
+				index++;
+
+				if (index == argc)
+				{
+					fprintf(stderr,
+						"***** Error: A value must be supplied when setting the sampling level.\n"
+					);
+
+					return false;
+				}
+
+				if (sscanf(argv[index], "%d", &samplingLevel) < 1)
+				{
+					fprintf(stderr,
+						"***** Error: '%s' is not a valid integer value (reading -a option).\n",
+						argv[index]
+					);
+
+					return false;
+				}
+			break;
+
+			case 'f':
+				index++;
+				outputFileName = argv[index];
+			break;
+
+			case 'i':
+				// dummy case because this is valid but not processed here.
+			break;
+
+			default:
 				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the left column.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &xMin) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -l option).\n",
+					"***** Error: '%s' is not a valid option.\n",
 					argv[index]
 				);
 
 				return false;
-			}
-		}
-		else if (0 == strcmp("-r", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the right column.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &xMax) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -r option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-		}
-		else if (0 == strcmp("-t", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the top row.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &yMin) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -t option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-		}
-		else if (0 == strcmp("-b", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the bottom row.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &yMax) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -b option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-		}
-		else if (0 == strcmp("-w", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the width.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &xMax) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -w option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-
-			xMax--;
-			xMin = 0;
-		}
-		else if (0 == strcmp("-h", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the height.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &yMax) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -h option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-
-			yMax--;
-			yMin = 0;
-		}
-		else if (0 == strcmp("-a", argv[index]))
-		{
-			index++;
-
-			if (index == argc)
-			{
-				fprintf(stderr,
-					"***** Error: A value must be supplied when setting the sampling level.\n"
-				);
-
-				return false;
-			}
-
-			if (sscanf(argv[index], "%d", &samplingLevel) < 1)
-			{
-				fprintf(stderr,
-					"***** Error: '%s' is not a valid integer value (reading -a option).\n",
-					argv[index]
-				);
-
-				return false;
-			}
-		}
-		else if (0 == strcmp("-f", argv[index]))
-		{
-			index++;
-			outputFileName = argv[index];
+			break;
 		}
 	}
 
-	if (xMin > xMax)
+	if (xMin == INT_MAX)
 	{
-		fprintf(stderr, "***** Error: Both left and right columns must be set. Use the -l and -r options, or the -w option.\n");
+		fprintf(stderr, "***** Error: Left column must be set. Use the -l or -w option.\n");
 
 		return false;
 	}
 
-	if (yMin > yMax)
+	if (xMax == INT_MIN)
 	{
-		fprintf(stderr, "***** Error: Both top and bottom rows must be set. Use the -t and -b options, or the -h option.\n");
+		fprintf(stderr, "***** Error: Right column must be set. Use the -r or -w option.\n");
+
+		return false;
+	}
+
+	if (yMin == INT_MAX)
+	{
+		fprintf(stderr, "***** Error: Top row must be set. Use the -t or -h option.\n");
+
+		return false;
+	}
+
+	if (yMax == INT_MIN)
+	{
+		fprintf(stderr, "***** Error: Bottom row must be set. Use the -b or -h option.\n");
 
 		return false;
 	}
 
 	if (NULL == outputFileName)
 	{
-		fprintf(stderr, "***** Error: An output file name must be set. Use the -f option to set the output file name.\n");
+		fprintf(stderr, "***** Error: An output file name must be set. Use the -f option.\n");
 
 		return false;
 	}
